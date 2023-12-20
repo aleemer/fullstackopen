bdb7f3f93f56cbed5728c3f9487411e1b8901070
aleemer <aleemertariq@gmail.com>
2023-12-20 12:22:50 -0800

    Added footer.

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -2,17 +2,29 @@
 const Hello = (props) => {
   return (
     <div>
-      <p> Hello {props.name} </p>
+      <p> Hello {props.name}, you are {props.age} years old </p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by Aleem under direction of <a href="https://github/mluukkai">mluukkai</a>
     </div>
   )
 }
 
 const App = () => {
+  const name = 'Peter';
+  const age = 10;
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name={'Aleem'}/>
-      <Hello name={'Joe'}/>
+      <Hello name={'Aleem'} age={26}/>
+      <Hello name={name} age={age}/>
+      <Footer />
     </div>
   )
 }
