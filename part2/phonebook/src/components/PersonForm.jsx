66c62c4c1366eb4f6cff9d4941208ac9af06e77b
aleemer <aleemertariq@gmail.com>
2023-12-22 12:06:55 -0800

    Completed exercise 2.10, added regex.

--- part2/phonebook/src/components/PersonForm.jsx
+++ part2/phonebook/src/components/PersonForm.jsx
@@ -1,8 +1,8 @@
 const PersonForm = ({ onFormSubmit }) => {
   return (
     <form onSubmit={onFormSubmit}>
-        <div> name: <input id="name" name="name"/> </div>
-        <div> number: <input id="number" name="number"/> </div>
+        <div> name: <input id="name" name="name" required pattern="[a-zA-Z ]+"/> </div>
+        <div> number: <input id="number" name="number" required pattern="[0-9 -]+"/> </div>
         <div>
           <button type="submit">add</button>
         </div>
