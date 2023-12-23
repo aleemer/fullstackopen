c147fb45cbd157580d9747a2014b5689ed104378
aleemer <aleemertariq@gmail.com>
2023-12-23 14:37:52 -0800

    Completed exercise 3.1.

--- part3/phonebook-server/index.js
+++ part3/phonebook-server/index.js
@@ -1,1 +1,38 @@
-console.log('hello');
+const express = require('express')
+const app = express()
+
+app.use(express.json()) // Allows us to parse requests with JSON data
+
+const persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+// Server endpoint to handle GET requests to '/api/persons'
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+// Server endpoint to handle GET requests to '/'
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
