c5e34bbf9e603a01fc3006dbb3f7071e6462a4fd
aleemer <aleemertariq@gmail.com>
2023-12-23 14:00:59 -0800

    Added handling to get a note by id, and handling for not found errors.

--- part3/part3/index.js
+++ part3/part3/index.js
@@ -29,6 +29,18 @@ app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
+// Server endpoint to handle all GET requests of the form '/api/notes/something'
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => id === note.id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end();
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
