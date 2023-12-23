592d80c0f7edd499da38d482f6a34de58f68d814
aleemer <aleemertariq@gmail.com>
2023-12-22 20:25:30 -0800

    Appropriately fetching and parsing data as desired.

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,7 +1,43 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Search from './components/Search';
+
 const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState('');
+
+  // parses the data appropriately
+  const parseData = (element) => {
+    if (element) {
+      return {
+        name: element.name.common ? element.name.common : 'unknown',
+        capital: element.capital ? element.capital[0] : 'unknown',
+        languages: element.languages ? Object.values(element.languages) : 'unknown',
+        flag: element.flags.png ? element.flags.png : 'unknown',
+        geo: element.capitalInfo.latlng ? element.capitalInfo.latlng : 'unknown'
+      }
+    }
+  }
+
+  // fetch all data from the restcountries server
+  const fetchAllData = () => {
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then((response) => {
+        setCountries(response.data.map(parseData));
+      });
+  }
+
+  useEffect(fetchAllData, []);  // only fetch the data once on first load
+
+  const handleSearchUpdate = (e) => {
+    e.preventDefault()
+    setSearch(e.target.value);
+  }
+
   return (
     <div>
-      Hello
+      <Search search={search} onSearchUpdate={handleSearchUpdate} />
     </div>
   )
 }
---
+++ part2/countries/src/components/Countries.jsx
@@ -0,0 +1,25 @@
+const CountrySimple = () => {
+  return (
+    <div>
+      Country simple
+    </div>
+  )
+}
+
+const CountryDetailed = () => {
+  return (
+    <div>
+      Country detailed
+    </div>
+  )
+}
+
+const Countries = () => {
+  return (
+    <div>
+      Countries
+    </div>
+  )
+}
+
+export default Countries
