66ffede966c587c4f111952938a577d0b2bfddaa
aleemer <aleemertariq@gmail.com>
2023-12-23 14:05:24 -0800

    Added deletion endpoint.

--- part3/part3/index.js
+++ part3/part3/index.js
@@ -41,6 +41,14 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
+// Server endpoint to handle all DELETE requests of the form '/api/notes/something'
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end();
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
