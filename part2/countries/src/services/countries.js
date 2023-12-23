e058d98283af7e07a129aeb342e1e6347e7ab45f
aleemer <aleemertariq@gmail.com>
2023-12-22 20:34:19 -0800

    extracted the requests and processing to a separate module.

---
+++ part2/countries/src/services/countries.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const restCountriesURL = 'https://studies.cs.helsinki.fi/restcountries'
+
+const parseData = (elem) => {
+  if (elem) {
+    return {
+      name: elem.name.common ? elem.name.common : 'unknown',
+      capital: elem.capital ? elem.capital[0] : 'unknown',
+      languages: elem.languages ? Object.values(elem.languages) : 'unknown',
+      flag: elem.flags.png ? elem.flags.png : 'unknown',
+      geo: elem.capitalInfo.latlng ? elem.capitalInfo.latlng : 'unknown'
+    }
+  }
+}
+
+const fetchAllData = () => {
+  const request = axios.get(`${restCountriesURL}/api/all`)
+  return request.then((response) => {
+    return response.data.map(parseData);
+  })
+}
+
+export default { fetchAllData }
