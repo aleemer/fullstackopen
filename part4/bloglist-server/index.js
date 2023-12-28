febc17cab916c0478f151e4689b5dd7d7b57861f
aleemer <aleemertariq@gmail.com>
2023-12-27 18:00:34 -0800

    Connection to bloglist-server database working...

--- part4/bloglist-server/index.js
+++ part4/bloglist-server/index.js
@@ -1,1 +1,50 @@
-console.log('hello');
+require("dotenv").config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+mongoose.set("strictQuery", false)
+
+// App setup
+app.use(cors())
+app.use(express.json())
+
+// Blog schema
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+const Blog = mongoose.model('Blog', blogSchema)
+
+// MongoDB Connection
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+  .then(() => console.log('successful connection to bloglist-server database',))
+  .catch((error) => console.log('error connecting...', error))
+
+
+// Endpoints
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+// Start server
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
