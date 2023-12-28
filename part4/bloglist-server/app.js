1d890fae6703031ac851cfcb614fc0a21e1cbf31
aleemer <aleemertariq@gmail.com>
2023-12-27 18:55:42 -0800

    Finished refactoring logger into utils.

--- part4/bloglist-server/app.js
+++ part4/bloglist-server/app.js
@@ -3,19 +3,21 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require("./controllers/blogs")
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
 mongoose.set("strictQuery", false)
 
+logger.info('connecting to...', config.MONGODB_URI)
+
 // MongoDB Connection
 mongoose.connect(config.MONGODB_URI)
-  .then(() => console.log('successful connection to MongoDB',))
-  .catch((error) => console.log('error connecting...', error.message))
+  .then(() => logger.info('successful connection to MongoDB'))
+  .catch((error) => logger.error('error connecting to MongoDB: ', error.message))
 
 // App setup
 app.use(cors())
 app.use(express.json())
-  // Add Blog router for endpoints to that resource
-app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', blogsRouter)    // Add Blog router for endpoints to that resource
 
 module.exports = app
