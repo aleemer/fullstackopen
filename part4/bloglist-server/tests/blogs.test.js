c029743d2af78165156f95f5844dcdf1f3ceb006
aleemer <aleemertariq@gmail.com>
2023-12-28 17:45:31 -0800

    Completed exercise 4.5.

--- part4/bloglist-server/tests/blogs.test.js
+++ part4/bloglist-server/tests/blogs.test.js
@@ -40,3 +40,37 @@ describe('total likes', () => {
     expect(result).toBe(15)
   })
 })
+
+describe('a favourite blog', () => {
+  const listWithZeroBlog = []
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+  const listWithManyBlogs = [
+    { title: 'Random Title 1', author: 'John Doe', url: 'https://example.com/1', likes: 5 },
+    { title: 'Random Title 2', author: 'Jane Smith', url: 'https://example.com/2', likes: 3 },
+    { title: 'Random Title 3', author: 'Bob Johnson', url: 'https://example.com/3', likes: 7 }
+  ]
+
+  test('of empty list is empty', () => {
+    const result = listHelper.favouriteBlog(listWithZeroBlog)
+    expect(listHelper.sameBlog(result, {})).toBe(true)
+  })
+
+  test('of list with one blog is that blog', () => {
+    const result = listHelper.favouriteBlog(listWithOneBlog)
+    expect(listHelper.sameBlog(listWithOneBlog[0], result)).toBe(true)
+  })
+
+  test('of list with many blogs', () => {
+    const result = listHelper.favouriteBlog(listWithManyBlogs)
+    expect(listHelper.sameBlog(listWithManyBlogs[2], result)).toBe(true)
+  })
+})
--- part4/bloglist-server/utils/list_helper.js
+++ part4/bloglist-server/utils/list_helper.js
@@ -2,6 +2,34 @@ const dummy = (blogs) => {
   return 1
 }
 
+const indexOfMax = (array) => {
+  if (array.length === 0) {
+    return -1
+  }
+
+  let max = array[0]
+  let maxIndex = 0
+  
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] >= max) {
+      maxIndex = i
+      max = array[i]
+    }
+  }
+  return maxIndex
+}
+
+const sameBlog = (a, b) => {
+  // Blog equality determined on all fields being the same
+  const fields = Object.keys(a)
+  fields.forEach((field) => {
+    if (a[field] !== b[field]) {
+      return false
+    }
+  })
+  return true
+}
+
 const totalLikes = (blogs) => {
   const reducer = (sum, blog) => {
     return sum + blog.likes
@@ -9,7 +37,14 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favouriteBlog = (blogs) => {
+  const indexMaxLikes = indexOfMax(blogs.map((blog) => blog.likes))
+  return indexMaxLikes === -1 ? {} : blogs[indexMaxLikes]
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  sameBlog,
+  totalLikes,
+  favouriteBlog
 }
