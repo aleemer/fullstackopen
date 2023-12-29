25e90ca2815fc566e311534cfa218e7a2c922921
aleemer <aleemertariq@gmail.com>
2023-12-28 17:18:24 -0800

    Completed exercise 4.4.

--- part4/bloglist-server/tests/blogs.test.js
+++ part4/bloglist-server/tests/blogs.test.js
@@ -6,3 +6,37 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const listWithZeroBlog = []
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+  const listWithManyBlogs = [
+    { _id: '', title: '', author: '', url: '', likes: 5, __v: 0 },
+    { _id: '', title: '', author: '', url: '', likes: 3, __v: 0 },
+    { _id: '', title: '', author: '', url: '', likes: 7, __v: 0 }
+  ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes(listWithZeroBlog)
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listWithManyBlogs)
+    expect(result).toBe(15)
+  })
+})
