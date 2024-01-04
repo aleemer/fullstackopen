59e717e30ac57f60fb27d3b9f2071b7e521f44eb
aleemer <aleemertariq@gmail.com>
2024-01-04 02:00:39 -0800

    Completed exercise 5.14

--- part5/bloglist-full-v1/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-full-v1/bloglist-frontend/src/components/Blog.test.js
@@ -26,3 +26,29 @@ test('only render title and author by default', () => {
   expect(likes).not.toBeInTheDocument()
   expect(url).not.toBeInTheDocument()
 })
+
+test('blog URL and number of likes now visible that button clicked', async () => {
+  const blog = {
+    title: 'This is a blog',
+    author: 'Ron Jeffries',
+    url: 'http://www.test.com',
+    user: {
+      name: 'John Doe',
+      username: 'J'
+    },
+    likes: 2
+  }
+
+  render(<Blog blog={blog} user={blog.user}/>)
+
+  // Perform click
+  const user = userEvent.setup()
+  const button = screen.queryByText('show')
+  await user.click(button)
+
+  // Expect likes and url to be defined
+  const likes = screen.queryByText(`likes ${blog.likes}`)
+  const url = screen.queryByText(`${blog.url}`)
+  expect(likes).toBeInTheDocument()
+  expect(url).toBeInTheDocument()
+})
