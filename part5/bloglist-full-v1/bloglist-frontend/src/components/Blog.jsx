f4379b8f0c2b04eb904c0025f91c6473578677f9
aleemer <aleemertariq@gmail.com>
2024-01-03 17:04:31 -0800

    Completed exercise 5.7.

--- part5/bloglist-full-v1/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-full-v1/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [show, setShow] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const handleClickShow = (e) => {
+    e.preventDefault()
+    setShow(!show)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={handleClickShow}>{show ? 'hide' : 'show'}</button>
+      </div>
+      {show &&
+      <>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes}<button>like</button></div>
+        <div>{blog.user.name}</div>
+      </>}
+  </div>
+)}
 
 export default Blog
